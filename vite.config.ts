da7035eb22a1ff49712553f7e3954873862e1dae
Egor Sychev <xvoddd@yandex.ru>
2022-05-13 22:23:03 +0300

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import Unocss from 'unocss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import Unocss from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Unocss()]
-})
+  plugins: [react(), Unocss()],
+});
